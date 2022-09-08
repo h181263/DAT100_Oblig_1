c37dbe91dcc942c52c1604980cb4062c3938a671
h181263 <111426453+h181263@users.noreply.github.com>
2022-09-08 13:10:45 +0200

    Add files via upload

---
+++ O3_heiltal.java
@@ -0,0 +1,23 @@
+package no.hvl.dat100;
+import static javax.swing.JOptionPane.*;
+import static java.lang.System.*;
+
+
+public class O3_heiltal {
+	public static void main(String[] args) {
+		int n = 0;
+		int fak = (n-1)*n; //opphøgd?
+		
+		//iterasjon
+		
+		
+		//output fak
+		
+		
+		
+		
+				
+		}
+	}
+
+
