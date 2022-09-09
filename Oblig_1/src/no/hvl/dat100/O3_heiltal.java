026dee5e5c21e74fa9053402706c04fd3301bb08
h181263 <111426453+h181263@users.noreply.github.com>
2022-09-09 22:25:41 +0200

    Update O3_heiltal.java

--- Oblig_1/src/no/hvl/dat100/O3_heiltal.java
+++ Oblig_1/src/no/hvl/dat100/O3_heiltal.java
@@ -1,17 +1,31 @@
 package no.hvl.dat100;
 import static javax.swing.JOptionPane.*;
+import java.lang.Integer.*;
+import javax.swing.JOptionPane;
+
 import static java.lang.System.*;
 
 
 public class O3_heiltal {
 	public static void main(String[] args) {
-		int n = 0;
-		int fak = (n-1)*n; //opphøgd?
+		int n = 1;
+		int fak = (n-1)*n;
 		
-		//iterasjon
+		//skrive inn heiltal
+		//feilmelding dersom ikkje heiltal
+		//dialogboks bruker skrive inn sin poengsum
+				String heiltal;
+				heiltal = JOptionPane.showInputDialog("Skriv inn eit heiltal: ");
+				int konvertert = Integer.parseInt(heiltal);
+		
+		//få ut fakultet
+				if (konvertert //er heiltal) {
+					System.out.println( //"Skriv ut fakultat ");
+				} else if (poengsum <=49 && poengsum >=40) {
+					System.out.println("Din karakter er E");
 		
 		
-		//output fak
+	
 		
 		
 		
